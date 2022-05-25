ac61c4ead31a5ceb77bfd008f0eb67fa09179c7b
ZhiangWang033 <zhw033@ucsd.edu>
2022-05-25 12:52:42 -0700

    removed

--- SpecPart/SpectralCommunityDetection/cmg/src/CombinatorialMultigrid.jl
+++
@@ -1,9 +0,0 @@
-module CombinatorialMultigrid
-    using SparseArrays
-    using LinearAlgebra
-    using LDLFactorizations
-    using Laplacians
-
-    include("cmgAlg.jl")
-    export cmg_preconditioner_adj, cmg_preconditioner_lap, lPreconditioner
-end
--- SpecPart/WriteClusters.jl
+++
@@ -1,9 +0,0 @@
-function WriteClusters(community::Vector{Int})
-    f = open("clusters.dat", "w")
-    
-    for i in 1:length(community)
-        println(f, i, ",", community[i])
-    end
-
-    close(f)
-end
